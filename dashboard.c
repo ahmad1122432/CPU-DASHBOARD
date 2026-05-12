581375827790e0cae4176d6cbfd58102d4e250af
ahmad1122432 <ahmadasghar20007@gmail.com>
2026-05-12 20:42:50 +0500

    updated

--- dashboard.c
+++ dashboard.c
@@ -1,1 +1,35 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+void clear_screen() {
+    // Clear the console screen
+    system("clear"); // For Unix/Linux systems
+    // system("cls"); // For Windows systems
+}
+
+int main()
+{
+    int cpu_usage = 0;
+    int memory_usage = 0;
+    int processes =5;
+
+    while(1)
+    {
+        clear_screen();
+        cpu_usage = rand() % 100; // Simulate CPU usage between 0-99%
+        memory_usage = rand() % 100; // Simulate Memory usage between 0-99%
+        processes = rand() % 20; // Simulate number of processes between 1-19
+
+        printf("==============================\n");
+        printf("   System Dashboard\n");
+        printf("==============================\n");
+        printf("CPU Usage: %d%%\n", cpu_usage);
+        printf("Memory Usage: %d%%\n", memory_usage);
+        printf("Running Processes: %d\n", processes);
+        printf("==============================\n");
+        sleep(1); // Update every 1 seconds
+
+    }
+    return 0;
+}
